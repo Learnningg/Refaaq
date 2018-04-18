eb33d30ee7764859c99024a0f07fb40406e2e9e6
elashry <mohamedelashry323@gmail.com>
2018-04-18 14:46:53 +0200

    Initial commit

---
+++ app/src/main/java/com/alatheer/refaaq/RegisterFamilyActivity.java
@@ -0,0 +1,14 @@
+package com.alatheer.refaaq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterFamilyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_family);
+
+    }
+}
