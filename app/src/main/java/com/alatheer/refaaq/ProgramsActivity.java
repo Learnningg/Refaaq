d6e0d46ffb99bc5436b6f5103ad11f6b1ab3dfe3
m <smasimo81@gmail.com>
2018-04-19 13:50:23 +0200

    jjj

---
+++ app/src/main/java/com/alatheer/refaaq/ProgramsActivity.java
@@ -0,0 +1,27 @@
+package com.alatheer.refaaq;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+
+public class ProgramsActivity extends AppCompatActivity {
+    String [] programsNames = {"البرنامج السابع","البرنامج السادس","البرنامج الخامس","البرنامج الرابع","البرنامج الثالث","البرنامج الثانى","البرنامج الاول "};
+    String [] programsRate = {"25$","100$","94$","25$","100$","94$","100$"};
+    int [] programsImg = {R.drawable.program5,R.drawable.program2,R.drawable.program3,R.drawable.program7,R.drawable.program5,R.drawable.program6,R.drawable.program7};
+    RecyclerView programRecyclerView;
+    ProgramsAdapter programRecyclerAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programs);
+        programRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_id);
+        programRecyclerAdapter = new ProgramsAdapter(ProgramsActivity.this,programsNames,programsRate,programsImg);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        programRecyclerView.setLayoutManager(layoutManager);
+        programRecyclerView.setHasFixedSize(true);
+        programRecyclerView.setAdapter(programRecyclerAdapter);
+    }
+}
