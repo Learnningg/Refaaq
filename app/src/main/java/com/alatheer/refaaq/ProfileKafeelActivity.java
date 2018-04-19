97308d865d67e9eea1aeef9f75a0f77d19c8f047
m <smasimo81@gmail.com>
2018-04-19 10:51:42 +0200

    ss

--- app/src/main/java/com/alatheer/refaaq/ProfileKafeelActivity.java
+++ app/src/main/java/com/alatheer/refaaq/ProfileKafeelActivity.java
@@ -1,13 +1,52 @@
 package com.alatheer.refaaq;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileKafeelActivity extends AppCompatActivity {
-
+public class ProfileKafeelActivity extends AppCompatActivity implements View.OnClickListener {
+Button profile,aytam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_kafeel);
+
+        initView();
+    }
+
+    private void initView() {
+        profile=findViewById(R.id.btn_profile_kafeel);
+        aytam=findViewById(R.id.btn_aytam_kafeel);
+
+        profile.setOnClickListener(this);
+        aytam.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.btn_profile_kafeel:
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentkafeel,new FragmentProfileKaffel())
+                        .commit();
+
+                    break;
+
+            case R.id.btn_aytam_kafeel:
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentkafeel,new FragmentAytamKaffel())
+                        .commit();
+
+                break;
+
+
+        }
     }
 }
